8334c7e2c543f57af3b36edfc323fc4e26eb1326
Benedict Lau <ben.hy.lau@gmail.com>
2018-07-02 21:12:51 -0400

    Fix IPFS mirrors

--- terraform/ipfs-mirror/ipfs-pin-service.sh
+++ terraform/ipfs-mirror/ipfs-pin-service.sh
@@ -2,7 +2,8 @@
 
 while true; do
   # Download m3u8 and pin IPFS content
-  wget -O- http://ipfs-server.__DOMAIN_NAME__/live.m3u8 2>/dev/null | grep http | tail -n 5 | awk -F '/' '{print $5}' | xargs -n 1 /root/ipfs-pin.sh
+  wget -q http://ipfs-server.__DOMAIN_NAME__/live.m3u8 -O /var/www/html/live.m3u8
+  cat /var/www/html/live.m3u8 | grep http | tail -n 5 | awk -F '/' '{print $5}' | xargs -n 1 /root/ipfs-pin.sh
 
   # Use IPNS to resolve m3u8 (uncomment to enable)
   #ipfs resolve /ipns/__IPFS_SERVER_IPFS_ID__ | ipfs cat | grep http | tail -n 5 | awk -F '/' '{print $5}' | xargs -n 1 /root/ipfs-pin.sh
