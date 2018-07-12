aff71796dfc994374341ab7f8a10445f88a615c2
Benedict Lau <ben.hy.lau@gmail.com>
2018-07-12 00:51:01 -0400

    Add fixes to nginx with https

--- terraform/ipfs-server/bootstrap.sh
+++ terraform/ipfs-server/bootstrap.sh
@@ -107,11 +107,18 @@ sed -i "s#__M3U8_HTTP_URLS__#${M3U8_HTTP_URLS}#g" /var/www/html/js/common.js
 cp -f /tmp/ipfs-server/default-no-ssl /etc/nginx/sites-available/default
 systemctl restart nginx.service
 
-#sed -i "s#__DOMAIN_NAME__#${DOMAIN_NAME}#g" /etc/nginx/sites-available/default
+# Configure nginx with HTTPS
+cp -f /tmp/ipfs-server/default /etc/nginx/sites-available/default
+sed -i "s#__DOMAIN_NAME__#${DOMAIN_NAME}#g" /etc/nginx/sites-available/default
 
-# Configure letsencrypt with certbot
-#openssl dhparam –out /etc/ssl/certs/dhparam.pem 2048
-#certbot certonly -n --agree-tos --standalone --email "${EMAIL_ADDRESS}" -d "${DOMAIN_NAME}" -d "ipfs-server.${DOMAIN_NAME}"
-#echo "30 2 * * 1 certbot renew >> /var/log/letsencrypt/letsencrypt.log" >> /etc/crontab
-#echo "35 2 * * 1 systemctl reload nginx" >> /etc/crontab
-#systemctl restart nginx.service
+# Generate dhparam.pem
+openssl dhparam –out /etc/ssl/certs/dhparam.pem 2048
+
+# Get letsencrypt certificates with certbot
+systemctl stop nginx.service
+certbot certonly -n --agree-tos --standalone --email "${EMAIL_ADDRESS}" -d "${DOMAIN_NAME}" -d "ipfs-server.${DOMAIN_NAME}"
+systemctl start nginx.service
+
+# Configure auto-renewals
+echo "30 2 * * 1 certbot renew >> /var/log/letsencrypt/letsencrypt.log" >> /etc/crontab
+echo "35 2 * * 1 systemctl reload nginx" >> /etc/crontab
