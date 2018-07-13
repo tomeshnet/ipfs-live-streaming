a74eede4ef4517d21ad996f4872f86d0b3fd9461
Garry Ing <garry.ing@gmail.com>
2018-07-12 22:08:44 -0400

    upstreamed edits

--- terraform/shared/video-player/js/common.js
+++ terraform/shared/video-player/js/common.js
@@ -1,34 +1,36 @@
 // JavaScript Document
-var ipfs_gateway_self='__IPFS_GATEWAY_SELF__';  // IPFS gateway of this node
-var ipfs_gateway_origin='__IPFS_GATEWAY_ORIGIN__';  // IPFS gateway of origin stream
-var m3u8_ipfs='live.m3u8';  // File path to m3u8 with IPFS content via HTTP server
-// var m3u8_ipfs='__IPFS_GATEWAY_ORIGIN__/ipns/__IPFS_ID_ORIGIN__';  // URL to m3u8 via IPNS (uncomment to enable)
-var m3u8_http_urls=[__M3U8_HTTP_URLS__];  // Optional list of URLs to m3u8 over HTTP
+
+var domain = 'ipfs01.vz.jgndata.biz';
+var ipfs_gateway_self = 'http://' + domain + ':8080'; // IPFS gateway of this node
+var ipfs_gateway_origin = 'http://ipfs-server.mesh.world:8080'; // IPFS gateway of origin stream
+var m3u8_ipfs = 'http://' + domain + '/live.m3u8'; // File path to m3u8 with IPFS content via HTTP server
+// var m3u8_ipfs = '/ipns/__IPFS_ID_ORIGIN__'; // URL to m3u8 via IPNS (uncomment to enable)
+var m3u8_http_urls = ['https://stream.aseriesoftubez.com/hls/tomesh/ournetworks.m3u8']; // Optional list of URLs to m3u8 over HTTP
 
 function getQueryVariable(variable) {
   var query = window.location.search.substring(1);
   var vars = query.split('&');
   for (var i = 0; i < vars.length; i++) {
     var pair = vars[i].split('=');
-    if (decodeURIComponent(pair[0]) == variable) {
+    if (decodeURIComponent(pair[0]) === variable) {
       return decodeURIComponent(pair[1]);
     }
   }
   console.log('Query variable %s not found', variable);
 }
 
-if (getQueryVariable("url")) {
-  m3u8_ipfs = getQueryVariable("url");
+if (getQueryVariable('url')) {
+  m3u8_ipfs = getQueryVariable('url');
 }
 
-if (getQueryVariable("ipfs_gateway_self")) {
-  ipfs_gateway_self = getQueryVariable("ipfs_gateway_self");
+if (getQueryVariable('ipfs_gateway_self')) {
+  ipfs_gateway_self = getQueryVariable('ipfs_gateway_self');
 }
 
 var live = videojs('live');
 
 // For any browser except Safari
-if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) == false) {
+if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) === false) {
   // Override native player for platform and browser consistency
   videojs.options.html5.nativeAudioTracks = false;
   videojs.options.html5.nativeVideoTracks = false;
@@ -38,7 +40,7 @@ if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) == false) {
 function httpStream() {
   live.src({
     src: m3u8_http_urls[Math.floor(Math.random() * m3u8_http_urls.length)],
-    type: 'application/x-mpegURL',
+    type: 'application/x-mpegURL'
   });
   loadStream();
 }
@@ -46,15 +48,15 @@ function httpStream() {
 function ipfsStream() {
   live.src({
     src: m3u8_ipfs,
-    type: 'application/x-mpegURL',
+    type: 'application/x-mpegURL'
   });
   loadStream();
   videojs.Hls.xhr.beforeRequest = function(options) {
     // Replace IPFS gateway of origin with that of this node
     options.uri = options.uri.replace(ipfs_gateway_origin, ipfs_gateway_self);
-    if (options.uri.indexOf("/ipfs/")) {
-      document.getElementById("loadingTitle").innerHTML = "Located stream via IPFS"
-      document.getElementById("msg").innerHTML = "Downloading video content..."
+    if (options.uri.indexOf('/ipfs/')) {
+      document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS';
+      document.getElementById('msg').innerHTML = 'Downloading video content...';
     }
     console.debug(options.uri);
     return options;
@@ -62,35 +64,43 @@ function ipfsStream() {
 }
 
 function loadStream() {
-  document.getElementById("LoadingStream").style.display = "block";
-  document.getElementById("SelectStream").style.display = "none";
+  document.getElementById('LoadingStream').style.display = 'block';
+  document.getElementById('SelectStream').style.display = 'none';
 }
 
-document.querySelector('.ipfs-stream').addEventListener('click', function(event){
+document.querySelector('.ipfs-stream').addEventListener('click', function(event) {
   ipfsStream();
 });
 
-document.querySelector('.http-stream').addEventListener('click', function(event){
+document.querySelector('.http-stream').addEventListener('click', function(event) {
   httpStream();
 });
 
-live.metadata="none";
+live.metadata = 'none';
 
 live.on('loadedmetadata', function() {
-  document.getElementById("StreamSelector").style.display = "none";
+  document.getElementById('StreamSelector').style.display = 'none';
 });
 
 live.on('loadeddata', function(event) {
   console.debug(event);
 });
 
-live.on('error', function(event) {
-  console.debug(this.error());
-  document.getElementById('loadingTitle').innerHTML = 'Unable to load live stream'
-  document.querySelector('.loader-animation').style.display = 'none';
-  document.getElementById('msg').innerHTML = this.error().message;
+var refreshButton = document.createElement('button');
+refreshButton.className = 'button button-primary compact stream-refresh';
+refreshButton.innerHTML = 'Refresh Page and Try Again';
+refreshButton.addEventListener('click', function() {
+  window.location.reload(true);
 });
 
-if (!m3u8_http_urls || !Array.isArray(m3u8_http_urls) || (m3u8_http_urls.length == 0)) {
+live.on('error', function(event) {
+  console.debug(this.error());
+  document.getElementById('loadingTitle').innerHTML = 'Unable to load live stream';
+  document.querySelector('.loader-animation').style.display = 'none';
+  document.getElementById('msg').innerHTML = this.error().message;
+  document.getElementById('LoadingStream').appendChild(refreshButton);
+});
+
+if (!m3u8_http_urls || !Array.isArray(m3u8_http_urls) || (m3u8_http_urls.length === 0)) {
   document.querySelector('.http-stream').setAttribute('disabled', 'disabled');
 }
