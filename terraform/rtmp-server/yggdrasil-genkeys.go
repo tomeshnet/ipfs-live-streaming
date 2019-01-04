1da4d850c2780cbb5df348a2b0cdbee22cafbb76
Benedict Lau <ben.hy.lau@gmail.com>
2019-01-04 17:28:23 -0500

    Improve genkeys code

--- terraform/rtmp-server/yggdrasil-genkeys.go
+++ terraform/rtmp-server/yggdrasil-genkeys.go
@@ -5,12 +5,9 @@ import "fmt"
 import "net"
 import "github.com/yggdrasil-network/yggdrasil-go/src/address"
 import "github.com/yggdrasil-network/yggdrasil-go/src/crypto"
-import "github.com/yggdrasil-network/yggdrasil-go/src/yggdrasil"
 
 func main() {
-  var core yggdrasil.Core
-
-  bpub, bpriv := core.NewEncryptionKeys()
+  bpub, bpriv := crypto.NewBoxKeys()
   id := crypto.GetNodeID(bpub)
   addr := net.IP(address.AddrForNodeID(id)[:])
 
