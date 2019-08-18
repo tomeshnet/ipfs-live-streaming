4437a7c3eaac9a19c622d54e522d344e68bc8fde
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-08-17 22:18:34 -0400

    Videojs seek hack and seekFrom  (#88)
    Added seek DVR to playback and hacks to make it work

--- terraform/shared/video-player/js/common.js
+++ terraform/shared/video-player/js/common.js
@@ -19,6 +19,7 @@ function getURLParam(key) {
 var ipfs_gw = getURLParam('gw')     // Set IPFS gateway URL to override playback gateway
 var live_ipfs = getURLParam('live') // Set m3u8 file URL to override IPFS live stream
 var vod_ipfs = getURLParam('vod')   // Set IPFS content hash of mp4 file to play IPFS on-demand video stream
+var startFrom = getURLParam("startFrom"); // Timecode to start video playing from
 
 if (ipfs_gw) {
   ipfs_gateway = ipfs_gw;
@@ -39,15 +40,12 @@ if (vod_ipfs) {
 }
 
 // Configure video player
-var live = videojs('live');
+var live = videojs('live', { liveui: true });
 
-// For any browser except Safari
-if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent) === false) {
-  // Override native player for platform and browser consistency
-  videojs.options.html5.nativeAudioTracks = false;
-  videojs.options.html5.nativeVideoTracks = false;
-  videojs.options.hls.overrideNative = true;
-}
+// Override native player for platform and browser consistency
+videojs.options.html5.nativeAudioTracks = false;
+videojs.options.html5.nativeVideoTracks = false;
+videojs.options.hls.overrideNative = true;
 
 function httpStream() {
   live.src({
@@ -57,6 +55,9 @@ function httpStream() {
   loadStream();
 }
 
+// Counter to track video playback state
+var streamState = 0;
+
 function ipfsStream() {
   live.src({
     src: stream_url_ipfs,
@@ -64,12 +65,36 @@ function ipfsStream() {
   });
   loadStream();
   videojs.Hls.xhr.beforeRequest = function(options) {
-    // Replace IPFS gateway of origin with that of this node
-    if (options.uri.indexOf('/ipfs/')>0) {
+
+    // When .m3u8 is loaded, start playback and transition to streamState = 1
+    if (options.uri.indexOf('.m3u8') > 0) {
+      if (!streamState) {
+        live.play();
+        streamState = 1;
+      }
+    }
+
+    if (options.uri.indexOf('/ipfs/') > 0) {
       document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS';
       document.getElementById('msg').innerHTML = 'Downloading video content...';
+      // Use specified IPFS gateway by replacing it in the uri
       options.uri = ipfs_gateway + options.uri.substring(options.uri.indexOf('/ipfs/'));
+
+      // Wait for two .ts chunks to be loaded before applying seek action
+      if (streamState < 3) {
+        streamState++;
+        if (streamState == 3) {
+          if (!startFrom) {
+            // Seek to live after waiting 1 s
+            setTimeout(function() { live.liveTracker.seekToLiveEdge(); }, 1);
+          } else {
+            // Seek to startFrom time after waiting 1 s
+            setTimeout(function() { live.currentTime(startFrom); }, 1);
+          }
+        }
+      }
     }
+
     if (options.uri.indexOf('/ipns/')>0) {
       document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS';
       document.getElementById('msg').innerHTML = 'Downloading video content...';
