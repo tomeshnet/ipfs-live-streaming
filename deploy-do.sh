0070bf3c28596d60b6df55efe2e88474ac921659
Benedict Lau <b@deprecated.systems>
2019-08-17 20:04:41 -0400

    Update deploy-do.sh

--- deploy-do.sh
+++ deploy-do.sh
@@ -2,38 +2,27 @@
 cd terraform
 
 echo "--------Domain Name--------"
-read -p "What is your domain name: " -r
+read -p "What is your domain name (e.g. tomesh.net): " -r
 echo -n $REPLY > .keys/domain_name
 
-echo Make sure you point your domain dns servers to
-echo ns1.digitalocean.com
-echo ns2.digitalocean.com
-echo ns3.digitalocean.com
+echo "Make sure the nameserver of your domain points to:"
+echo "ns1.digitalocean.com"
+echo "ns2.digitalocean.com"
+echo "ns3.digitalocean.com"
 
-
-echo "--------Digital Oceans API Token--------"
-read -p "What is your API Token form DigitalOcean: "  -r
+echo "--------DigitalOcean API Token--------"
+read -p "What is your DigitalOcean API Token: "  -r
 echo -n $REPLY > .keys/do_token
 
-echo "--------Digital Oceans RSA--------"
-echo "Press enter when prompted for password "
-ssh-keygen -t rsa -f .keys/id_rsa
-echo Add the SSH key to your Digital Ocean account under Settings > Security
-echo "------------------"
-cat  .keys/id_rsa.pub
-echo "------------------"
+echo "--------DigitalOcean SSH Keys--------"
+echo "Creating RSA keys for SSH access..."
+ssh-keygen -q -t rsa -f .keys/id_rsa
 fingerprint=$(ssh-keygen -l -E md5 -f .keys/id_rsa.pub | awk '{print $2}' | sed 's/MD5://')
-echo -n $fingerprint> .keys/ssh_fingerprint
-echo Once added the finger print will be $fingerprint
+echo -n $fingerprint > .keys/ssh_fingerprint
 
-echo "--------Lets Encrypt--------"
-read -p "What is your e-mail (for lets encrypt): "  -r
+echo "Add the newly created SSH public key to your DigitalOcean account under 'Settings > Security' and verify the fingerprint '$fingerprint':"
+cat .keys/id_rsa.pub
+
+echo "--------Let's Encrypt SSL/TLS Certificates--------"
+read -p "What is the e-mail you would like to use for Let's Encrypt: " -r
 echo -n $REPLY > .keys/email_address
-
-echo "----Installing terraform-----"
-wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
-unzip terraform_0.11.7_linux_amd64.zip
-sudo mv terraform /usr/bin
-
-terraform init
-# terraform apply
