bb0587689e5194173ae7110a826a5401d899052e
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-08-23 13:29:42 -0400

    updated comment

--- terraform/ipfs-mirror/ipfs-pin-service.sh
+++ terraform/ipfs-mirror/ipfs-pin-service.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 while true; do
-  # Connect to the source ipfs server
+  # Re-establish swarm connection to main IPFS server
+  #    IPFS takes a long time to find the hashes. 
+  #    By manually setting a connection allows pinning to happen very quickly
+  #    Since connections are dropped we re-connect every loop
   ipfs swarm connect /ipv4/__IPFS_SERVER_IPFS_IP__/tcp/ipfs/__IPFS_SERVER_IPFS_ID__
   # Download m3u8 and pin IPFS content
   wget -q https://ipfs-server.__DOMAIN_NAME__/live.m3u8 -O /var/www/html/live.m3u8
@@ -10,5 +13,5 @@ while true; do
   # Use IPNS to resolve m3u8 (uncomment to enable)
   #ipfs resolve /ipns/__IPFS_SERVER_IPFS_ID__ | ipfs cat | grep http | tail -n 5 | awk -F '/' '{print $5}' | xargs -n 1 /root/ipfs-pin.sh
 
-  sleep 5  
+  sleep 5
 done
