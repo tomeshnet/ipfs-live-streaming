bed2b48bd94e01e3e97dc912d46c43bc9aa75796
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-08-23 14:59:16 -0400

    Syntax cleanup

--- terraform/rtmp-server/process-stream.sh
+++ terraform/rtmp-server/process-stream.sh
@@ -17,28 +17,26 @@ while true; do
     timecode=$(grep -B1 ${nextfile} ${what}.m3u8 | head -n1 | awk -F : '{print $2}' | tr -d ,)
     if ! [ -z "${nextfile}" ]; then
       if ! [ -z "{$timecode}" ]; then
-
         reset_stream_marker=''
         if [[ "$(grep -B2 ${nextfile} ${what}.m3u8 | head -n1)" == "#EXT-X-DISCONTINUITY" || "${RESTARTED}" == "1" ]]; then
           reset_stream_marker=" #EXT-X-DISCONTINUITY"
-	  RESTARTED="0"
+          RESTARTED="0"
         fi
 
         # Current UTC date for the log
         time=$(date "+%F-%H-%M-%S")
 
         # Add ts file to IPFS
-        hash=`ipfs add -Q ${nextfile}`
-	
-	if [[ -z "${hash}" ]]; then
-	   echo ${nextfile} Add Failed, skipping for retry
-	else
+        hash=$(ipfs add -Q ${nextfile})
+
+        if [[ -z "${hash}" ]]; then
+          echo ${nextfile} Add Failed, skipping for retry
+        else
           # Update the log with the future name (hash already there)
           echo added ${hash} ${nextfile} ${time}.ts ${timecode}${reset_stream_marker} >>~/process-stream.log
 
           # Remove nextfile and tmp.txt
           rm -f ${nextfile} ~/tmp.txt
-
           echo "#EXTM3U" >current.m3u8
           echo "#EXT-X-VERSION:3" >>current.m3u8
           echo "#EXT-X-TARGETDURATION:20" >>current.m3u8
@@ -50,7 +48,7 @@ while true; do
           # Add m3u8 file to IPFS and IPNS publish (uncomment to enable)
           #m3u8hash=$(ipfs add current.m3u8 | awk '{print $2}')
           #ipfs name publish --timeout=5s $m3u8hash &
-
+          
           # Copy files to web server
           cp current.m3u8 /var/www/html/live.m3u8
           cp ~/process-stream.log /var/www/html/live.log
