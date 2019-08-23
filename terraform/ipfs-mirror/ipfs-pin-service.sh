f247b752bb77aad81e6c376974232b2f5cd7ec9b
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-08-23 12:55:40 -0400

    Connect to source ipfs server

--- terraform/ipfs-mirror/ipfs-pin-service.sh
+++ terraform/ipfs-mirror/ipfs-pin-service.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 while true; do
+  # Connect to the source ipfs server
+  ipfs swarm connect /ipv4/__IPFS_SERVER_IPFS_IP__/tcp/ipfs/__IPFS_SERVER_IPFS_ID__
   # Download m3u8 and pin IPFS content
   wget -q https://ipfs-server.__DOMAIN_NAME__/live.m3u8 -O /var/www/html/live.m3u8
   cat /var/www/html/live.m3u8 | grep http | tail -n 5 | awk -F '/' '{print $5}' | xargs -n 1 /root/ipfs-pin.sh
@@ -8,5 +10,5 @@ while true; do
   # Use IPNS to resolve m3u8 (uncomment to enable)
   #ipfs resolve /ipns/__IPFS_SERVER_IPFS_ID__ | ipfs cat | grep http | tail -n 5 | awk -F '/' '{print $5}' | xargs -n 1 /root/ipfs-pin.sh
 
-  sleep 1
+  sleep 5  
 done
