95d6713c9bea5e71970cc02c2c7d9090b546a404
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-08-23 20:18:34 -0400

    Process stream tweaks (#95)
    * Tweaks
    
    Sleep only to 1 second now 5
    Remove unneeded code
    
    * Set hls fragments to 20s
    
    * Restarted tweak and correct size to 20
    
    * More tweaks
    
    Sequence should always be 0 since its an event
    using -Q in ipfs to get the ipfs hash instead of piping out put to a file
    removed retry in favor of just looking again with the same filename to retry
    
    * Syntax cleanup

--- terraform/rtmp-server/process-stream.sh
+++ terraform/rtmp-server/process-stream.sh
@@ -7,60 +7,40 @@
 rm -rf ~/hls/*
 cd ~/hls
 
-DISCONNET=1
 what="stream1"
-M3U8_SIZE=5
+RESTARTED="1"
 
 while true; do
-	nextfile=$(cat ${what}.m3u8 | tail -n1)
+  nextfile=$(cat ${what}.m3u8 | tail -n1)
 
   if [ -f "${nextfile}" ]; then
     timecode=$(grep -B1 ${nextfile} ${what}.m3u8 | head -n1 | awk -F : '{print $2}' | tr -d ,)
     if ! [ -z "${nextfile}" ]; then
       if ! [ -z "{$timecode}" ]; then
-
         reset_stream_marker=''
-        if [[ "$(grep -B2 ${nextfile} ${what}.m3u8 | head -n1)" == "#EXT-X-DISCONTINUITY" ]]; then
+        if [[ "$(grep -B2 ${nextfile} ${what}.m3u8 | head -n1)" == "#EXT-X-DISCONTINUITY" || "${RESTARTED}" == "1" ]]; then
           reset_stream_marker=" #EXT-X-DISCONTINUITY"
+          RESTARTED="0"
         fi
 
         # Current UTC date for the log
         time=$(date "+%F-%H-%M-%S")
 
         # Add ts file to IPFS
+        hash=$(ipfs add -Q ${nextfile})
 
-        ret=$(
-          ipfs add ${nextfile} 2>/dev/null >~/tmp.txt
-          echo $?
-        )
-        attempts=5
-        until [[ ${ret} -eq 0 || ${attempts} -eq 0 ]]; do
-          # Wait and retry
-          sleep 1
-          ret=$(
-            ipfs add ${nextfile} 2>/dev/null >~/tmp.txt
-            echo $?
-          )
-          attempts=$((attempts - 1))
-        done
-        if [[ ${ret} -eq 0 ]]; then
+        if [[ -z "${hash}" ]]; then
+          echo ${nextfile} Add Failed, skipping for retry
+        else
           # Update the log with the future name (hash already there)
-          echo $(cat ~/tmp.txt) ${time}.ts ${timecode}${reset_stream_marker} >>~/process-stream.log
+          echo added ${hash} ${nextfile} ${time}.ts ${timecode}${reset_stream_marker} >>~/process-stream.log
 
           # Remove nextfile and tmp.txt
           rm -f ${nextfile} ~/tmp.txt
-
-          # Write the m3u8 file with the new IPFS hashes from the log
-          totalLines="$(wc -l ~/process-stream.log | awk '{print $1}')"
-
-          sequence=0
-          if (("${totalLines}" > ${M3U8_SIZE})); then
-            sequence=$(expr ${totalLines} - ${M3U8_SIZE})
-          fi
           echo "#EXTM3U" >current.m3u8
           echo "#EXT-X-VERSION:3" >>current.m3u8
-          echo "#EXT-X-TARGETDURATION:10" >>current.m3u8
-          echo "#EXT-X-MEDIA-SEQUENCE:${sequence}" >>current.m3u8
+          echo "#EXT-X-TARGETDURATION:20" >>current.m3u8
+          echo "#EXT-X-MEDIA-SEQUENCE:0" >>current.m3u8
           echo "#EXT-X-PLAYLIST-TYPE:EVENT" >>current.m3u8
 
           cat ~/process-stream.log | awk '{print $6"#EXTINF:"$5",\n'${IPFS_GATEWAY}'/ipfs/"$2}' | sed 's/#EXT-X-DISCONTINUITY#/#EXT-X-DISCONTINUITY\n#/g' >>current.m3u8
@@ -68,7 +48,7 @@ while true; do
           # Add m3u8 file to IPFS and IPNS publish (uncomment to enable)
           #m3u8hash=$(ipfs add current.m3u8 | awk '{print $2}')
           #ipfs name publish --timeout=5s $m3u8hash &
-
+          
           # Copy files to web server
           cp current.m3u8 /var/www/html/live.m3u8
           cp ~/process-stream.log /var/www/html/live.log
@@ -76,6 +56,6 @@ while true; do
       fi
     fi
   else
-    sleep 5
+    sleep 1
   fi
 done
