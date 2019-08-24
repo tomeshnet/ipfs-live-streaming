70c6b364e761181bdd5a4b3b11b79305b971b122
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-08-23 23:27:54 -0400

    Add player ability to calculate startFrom using IPFS hash (#94)
    * startFrom match up IPFS hashes
    * m3u8 url param 
    * remove redundant code

--- terraform/shared/video-player/js/common.js
+++ terraform/shared/video-player/js/common.js
@@ -1,42 +1,72 @@
 // IPFS config
-var ipfs_gateway = '__IPFS_GATEWAY_SELF__';     // IPFS gateway
+var ipfs_gateway = '__IPFS_GATEWAY_SELF__'; // IPFS gateway
 
 // Live stream config
 var m3u8_ipfs = 'live.m3u8';                                          // HTTP or local path to m3u8 file containing IPFS content
 // var m3u8_ipfs = '__IPFS_GATEWAY_ORIGIN__/ipns/__IPFS_ID_ORIGIN__'; // IPNS path to m3u8 file containing IPFS content (uncomment to enable)
 var m3u8_http_urls = [__M3U8_HTTP_URLS__];                            // HTTP or local paths to m3u8 file containing HTTP content (optional)
 
-// Configure default playback behaviour
-var stream_type = 'application/x-mpegURL'; // Type of video stream
-var stream_url_ipfs = m3u8_ipfs;           // Source of IPFS video stream
-var stream_urls_http = m3u8_http_urls;     // Source of HTTP video stream
-
 // Process URL params
 function getURLParam(key) {
   return new URLSearchParams(window.location.search).get(key);
 }
 
-var ipfs_gw = getURLParam('gw')     // Set IPFS gateway URL to override playback gateway
-var live_ipfs = getURLParam('live') // Set m3u8 file URL to override IPFS live stream
-var vod_ipfs = getURLParam('vod')   // Set IPFS content hash of mp4 file to play IPFS on-demand video stream
-var startFrom = getURLParam("startFrom"); // Timecode to start video playing from
+var ipfs_gw = getURLParam('gw')
+if (getURLParam('m3u8'))
+  var m3u8_ipfs = getURLParam('m3u8')   // Set m3u8 file URL to override IPFS live stream
+var vod_ipfs = getURLParam('vod')       // Set IPFS content hash of mp4 file to play IPFS on-demand video stream
+var start_from = getURLParam("from");   // Timecode to start video playing from
+
+// Configure default playback behaviour
+var stream_type = 'application/x-mpegURL'; // Type of video stream
+var stream_url_ipfs = m3u8_ipfs;           // Source of IPFS video stream
+var stream_urls_http = m3u8_http_urls;     // Source of HTTP video stream
 
 if (ipfs_gw) {
   ipfs_gateway = ipfs_gw;
-}
-
-if (live_ipfs) {
-  stream_type = 'application/x-mpegURL';
-  stream_url_ipfs = live_ipfs;
-  stream_urls_http = m3u8_http_urls;
 }
 
 if (vod_ipfs) {
   stream_type = 'video/mp4';
   stream_url_ipfs = ipfs_gateway + '/ipfs/' + vod_ipfs;
   stream_urls_http = [];
-
   document.getElementById('selectingTitle').innerHTML = 'Select Recorded Stream Source';
+}
+
+// If start_from is not a number it's probably an IPFS hash so calculate to correct start_from
+var hash="";
+if (start_from && +start_from != start_from) {
+  hash = start_from;
+  // Remove start_from value since the hash may not be in the list
+  start_from = undefined;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      file = this.response;
+      fileline = file.split("\n");
+      counter = 0;
+      // Loop through entries in the file
+      for (var a = 0; a < fileline.length; a++) {
+        // Look for EXTINF tags that describe the length of the chunk
+        if (fileline[a].indexOf("EXTINF:") > 0) {
+          // Parse out the length of the chunk
+          var number = fileline[a].substring(fileline[a].indexOf("EXTINF:") + 7);
+          number = number.substring(0, number.length - 1);
+          // Skip over chunk hash information
+          a++;
+          if (fileline[a].indexOf(hash) > 0) {
+            // If hash is found set the start_from to the counter and exit;
+            start_from = counter;
+            return;
+          }
+          // Add chunk length to counter
+          counter = counter + parseFloat(number);
+        }
+      }
+    }
+  };
+  xmlhttp.open("GET", m3u8_ipfs, true);
+  xmlhttp.send();
 }
 
 // Configure video player
@@ -84,18 +114,18 @@ function ipfsStream() {
       if (streamState < 3) {
         streamState++;
         if (streamState == 3) {
-          if (!startFrom) {
+          if (!start_from) {
             // Seek to live after waiting 1 s
             setTimeout(function() { live.liveTracker.seekToLiveEdge(); }, 1);
           } else {
-            // Seek to startFrom time after waiting 1 s
-            setTimeout(function() { live.currentTime(startFrom); }, 1);
+            // Seek to start_from time after waiting 1 s
+            setTimeout(function() { live.currentTime(start_from); }, 1);
           }
         }
       }
     }
 
-    if (options.uri.indexOf('/ipns/')>0) {
+    if (options.uri.indexOf('/ipns/') > 0) {
       document.getElementById('loadingTitle').innerHTML = 'Located stream via IPFS';
       document.getElementById('msg').innerHTML = 'Downloading video content...';
       options.uri = ipfs_gateway + options.uri.substring(options.uri.indexOf('/ipns/'));
