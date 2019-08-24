a2b095e277188b98be916e1646c607563489835f
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-08-23 23:07:40 -0400

    Update bootstrap-post-dns.sh

--- terraform/ipfs-mirror/bootstrap-post-dns.sh
+++ terraform/ipfs-mirror/bootstrap-post-dns.sh
@@ -5,6 +5,7 @@ set -e
 IPFS_MIRROR_INSTANCE=$1
 DOMAIN_NAME=$2
 EMAIL_ADDRESS=$3
+DRY_RUN=$4
 
 #######################
 # nginx + letsencrypt #
@@ -15,7 +16,11 @@ openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
 # Get letsencrypt certificates with certbot
 systemctl stop nginx.service
-certbot certonly -n --agree-tos --standalone --email "${EMAIL_ADDRESS}" -d "ipfs-mirror-${IPFS_MIRROR_INSTANCE}.${DOMAIN_NAME}" -d "ipfs-gateway-${IPFS_MIRROR_INSTANCE}.${DOMAIN_NAME}"
+if [[ "${DRY_RUN}" == "true" ]]; then
+  certbot certonly -n --dry-run --agree-tos --standalone --email "${EMAIL_ADDRESS}" -d "ipfs-mirror-${IPFS_MIRROR_INSTANCE}.${DOMAIN_NAME}" -d "ipfs-gateway-${IPFS_MIRROR_INSTANCE}.${DOMAIN_NAME}"
+else
+  certbot certonly -n --agree-tos --standalone --email "${EMAIL_ADDRESS}" -d "ipfs-mirror-${IPFS_MIRROR_INSTANCE}.${DOMAIN_NAME}" -d "ipfs-gateway-${IPFS_MIRROR_INSTANCE}.${DOMAIN_NAME}"
+fi
 
 # Configure nginx with HTTPS
 cp -f /tmp/ipfs-mirror/nginx-default /etc/nginx/sites-available/default
